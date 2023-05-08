6983d8c74142bce71854a73518a89c2125c0fe88
DiegoJZK <zanin.diego@gmail.com>
2023-05-08 19:30:26 -0300

    teste

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const routes = require("./routes/routes")
 const server = express()
 const PORT = 3000
 
+
 server.use(express.json())
 server.use(express.urlencoded({extended:true}))
 server.use(routes)
+
 
 
 server.listen(PORT,()=>{
